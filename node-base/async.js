2fddecc0fd0585ca75a1e0659910e528d35a4f10
Mabrur Habib <mabrurh@dksec.local>
2022-04-22 16:22:51 +0700

    async err

--- node-base/async.js
+++ node-base/async.js
@@ -9,6 +9,8 @@ console.log('File created...');
 
 //asynchronous way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('ERROR ❌');
+
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
         console.log(data2);
         fs.readFile('./txt/syncout.txt', 'utf-8', (err, data3) => {
